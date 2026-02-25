fffa7107b58a126fd3b939464b53f9cd66b67aeb
EverNorif <1320346985@qq.com>
2026-02-25 16:16:28 +0800

    use relative reach to refactor lift / pull / push skill.

--- source/autosim/autosim/skills/__init__.py
+++ source/autosim/autosim/skills/__init__.py
@@ -2,13 +2,16 @@ from isaaclab.utils import configclass
 
 from .base_skill import CuroboSkillExtraCfg
 from .gripper import GraspSkill, GraspSkillCfg, UngraspSkill, UngraspSkillCfg
-from .lift import LiftSkill, LiftSkillCfg
 from .navigate import NavigateSkill, NavigateSkillCfg, NavigateSkillExtraCfg
-from .press import PressSkill, PressSkillCfg
-from .pull import PullSkill, PullSkillCfg
-from .push import PushSkill, PushSkillCfg
 from .reach import ReachSkill, ReachSkillCfg
-from .rotate import RotateSkill, RotateSkillCfg
+from .relative_reach import (
+    LiftSkill,
+    LiftSkillCfg,
+    PullSkill,
+    PullSkillCfg,
+    PushSkill,
+    PushSkillCfg,
+)
 
 
 @configclass
@@ -19,11 +22,9 @@ class AutoSimSkillsExtraCfg:
     ungrasp: UngraspSkillCfg = UngraspSkillCfg()
     lift: LiftSkillCfg = LiftSkillCfg()
     moveto: NavigateSkillCfg = NavigateSkillCfg()
-    press: PressSkillCfg = PressSkillCfg()
     pull: PullSkillCfg = PullSkillCfg()
     push: PushSkillCfg = PushSkillCfg()
     reach: ReachSkillCfg = ReachSkillCfg()
-    rotate: RotateSkillCfg = RotateSkillCfg()
 
     def get(cls, skill_name: str):
         """Get the skill configuration by name."""
